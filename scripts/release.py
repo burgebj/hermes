c092ce47bfe5740b69b574bb15d8d6e6feb77405
qindongliang <18210507492@126.com>
2026-05-20 10:08:33 +0800

    chore(release): map qindongliang author email

--- scripts/release.py
+++ scripts/release.py
@@ -143,6 +143,7 @@ AUTHOR_MAP = {
     "zhekinmaksim@gmail.com": "Zhekinmaksim",
     "obafemiferanmi1999@gmail.com": "KvnGz",
     "159539633+MottledShadow@users.noreply.github.com": "MottledShadow",
+    "18210507492@126.com": "qindongliang",
     "aludwin+gh@gmail.com": "adamludwin",
     "ngusev@astralinux.ru": "NikolayGusev-astra",
     "liuguangyong201@hellobike.com": "liuguangyong93",
